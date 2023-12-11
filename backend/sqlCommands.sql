25f8190620e2680438b22fdc8fd44951cab21b67
Urrutia-308 <jose_urrutia@att.net>
2023-12-11 09:25:52 -0800

    sql tables

---
+++ backend/sqlCommands.sql
@@ -0,0 +1,24 @@
+use cs425;
+
+insert into cs425.tblMajor(majorID, majorName)
+values
+(1, 'Computer Science & Engineering'),
+(2, 'Civil Engineering'),
+(3, 'Physics'),
+(4, 'Accounting'),
+(5, "Chemistry"), 
+(6, "Finance"),
+(7, "Biology"),
+(8, "Psychology"),
+(9, "Business"),
+(10, "English");
+
+
+
+
+/*tblUser insert command*/
+insert into cs425.tblUser (Fname, Lname, DOB, Email, Passwd)
+values ('Jose', 'Urrutia', '1999-06-23', 'jurrutia@gmail.com', 'passEx123'),
+('Jessica', 'Aquil', '2005-10-12', 'aquil@gmail.com', 'urrutia45'),
+('Cindy', 'Portillo', '1989-02-23', 'jurrutia@gmail.com', 'djgh$kjj785'),
+('Dave', 'Smith', '1901-12-23', 'DSmith@gmail.com', 'SD23dg$');
