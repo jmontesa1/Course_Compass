bebc453ae25f69264ac6660cebccec134408d15f
Lucas Videtto <104482165+videttolucas@users.noreply.github.com>
2024-02-14 16:08:17 -0800

    Revised login function for backend; Sessioning

--- backend_README/README.md
+++
@@ -1,2 +0,0 @@
-# Course_Compass_T38
-Backend implementation for Team 38's Course Compass
--- frontend/src/main.js
+++
@@ -1,7 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import './main.css' //Global CSS for Font
-import NavBar from './components/NavBar.vue'
-
-createApp(App).use(router).mount('#app')
